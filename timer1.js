81515ce90d009a8522c4ecba70d379b870af38cf
Yves Candau <ycandau@gmail.com>
2021-06-29 06:11:04 +0000

    Added timer

---
+++ timer1.js
@@ -0,0 +1,16 @@
+const readInput = () =>
+  process.argv
+    .slice(2)
+    .map((str) => Number(str))
+    .filter((x) => !Number.isNaN() && x > 0);
+
+// Sound output not working
+const beep = () => console.log('Beep!');
+
+const setTimers = (delays) => {
+  delays.forEach((delay) => {
+    setTimeout(beep, delay * 1000);
+  });
+};
+
+setTimers(readInput());
